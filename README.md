eef840b59a97914b8a7424d1ac48f9d3a4aa0c77
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-12-02 00:27:41 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Advent of Code
+
+My solutions for [Advent of Code 2023](https://adventofcode.com/2023) in JavaScript.
+
+Run with:
+
+```sh
+npm test solutions/day-XX
+```
---
+++ solutions/day-00/index.js
@@ -0,0 +1,16 @@
+
+/**
+ * @param {string} input
+ * @returns {string}
+ */
+export function solution1(input) {
+	return input;
+}
+
+/**
+ * @param {string} input
+ * @returns {string}
+ */
+export function solution2(input) {
+	return input;
+}

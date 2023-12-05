23c8eddc7a7a1ef48a0b0f7815c306b3e34d73f8
Rowan Manning <138944+rowanmanning@users.noreply.github.com>
2023-12-05 21:44:23 +0000

    Properly type the intersection function

--- packages/array/index.js
+++ packages/array/index.js
@@ -1,9 +1,10 @@
 /**
  * Get an array of the common items which appear in two arrays.
  *
- * @param {any[]} array1 - The first array.
- * @param {any[]} array2 - The second array.
- * @returns {any[]} - Returns the intersection array.
+ * @template {any} Type1
+ * @param {Type1[]} array1 - The first array.
+ * @param {Type1[]} array2 - The second array.
+ * @returns {Type1[]} - Returns the intersection array.
  */
 export function intersection(array1, array2) {
 	return array1.filter((item) => array2.includes(item));
